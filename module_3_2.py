817a6c2a95972383149ab736ac3b3aa068eccfec
Olga <simonenkoolga715@gmail.com>
2024-12-11 21:57:19 +0400

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -7,3 +7,12 @@ def send_email(message,recipient,*,sender = "university.help@gmail.com"):
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+# Письмо успешно отправлено с адреса university.help@gmail.com на адрес vasyok1337@gmail.com
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+# НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса urban.info@gmail.com на адрес urban.fan@mail.ru
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+# Невозможно отправить письмо с адреса urban.teacher@mail.uk на адрес urban.student@mail.ru
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+# Нельзя отправить письмо самому себе!
